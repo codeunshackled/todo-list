f1018ccfc1d47a88c80a74218f8a8010c510f2c0
Leo <codeunshackled@gmail.com>
2021-11-28 13:13:36 -0300

    added remove, edit and checked features

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,34 +1,58 @@
 let searchForm = document.querySelector('.search-form');
 let input = document.querySelector('input');
 let list = document.querySelector('.todo-list');
+let clearButton = document.querySelector('.clear-button');
+let getItems = document.getElementsByClassName('edit-item');
 
 function createItem(content) {
-  let htmlLI = `<li class="is-clearfix">
+  let htmlLI = `<li  class="is-clearfix">
                   <span class="is-pulled-left">${content}</span>
-                  <span class="is-pulled-right">
-                    <span class="icon"><i class="fas fa-home"></i></span>
-                    <span class="icon"><i class="fas fa-edit"></i></span>
-                    <span class="icon"><i class="fas fa-times-circle"></i></span>
+                  <span onclick="editItem(event)" class="edit-item is-pulled-right">
+                    <span name="check" class="check icon fas fa-home is-medium"></span>
+                    <span name="edit" class="edit icon fas fa-edit is-medium"></span>
+                    <span name="remove" class="remove icon fas fa-times-circle is-medium"></span>
                   </span>
                 </li>`;
 
   list.insertAdjacentHTML('beforeend', htmlLI);
-
-
-  // creates element
-  // inserts content into element
-  // inserts element into list
-
 }
 
 searchForm.addEventListener('submit', function(e) {
   e.preventDefault();
   let inputText = e.target.querySelector('input');
   let captureMessage = inputText.value;
-  console.log(captureMessage);
   createItem(captureMessage)
   inputText.value = ' ';
-
-
-
 })
+
+clearButton.addEventListener('click', () => {
+  let clear = confirm("Do you really want to clear the list?");
+  if (clear)  list.innerHTML = ' ';
+});
+
+function editItem(event) {
+  let itemClass = event.target.classList;
+  let elm = event.currentTarget.parentElement.firstElementChild;
+  let elmParent = event.currentTarget.parentElement;
+
+  if (itemClass.contains('remove')) {
+    event.currentTarget.parentElement.remove();
+  } else if (itemClass.contains('check')) {
+    elm.style["text-decoration"] = "line-through";
+  } else if (itemClass.contains('edit')) {
+    let input = document.createElement('input');
+    input.classList = "is-pulled-left";
+    input.value = elm.textContent;
+    input.onkeyup = function(e){
+      if (e.key === 'Enter') {
+        let item = document.createElement('span');
+        item.classList = "is-pulled-left";
+        item.textContent = e.target.value;
+        elmParent.insertBefore(item, input);
+        input.remove();
+    }
+    };
+    event.currentTarget.parentElement.insertBefore(input, elm);
+    elm.remove();
+  }
+}
